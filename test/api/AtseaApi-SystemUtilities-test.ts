1cf62d8017da9a0e53ca8b8202c34b1559528c37
Cristian Mejia <crmejiam@unal.edu.co>
2021-03-14 20:34:24 -0500

    trying to fix database healthcheck test again

--- test/api/AtseaApi-SystemUtilities-test.ts
+++ test/api/AtseaApi-SystemUtilities-test.ts
@@ -7,7 +7,7 @@ const host = `http://3.142.215.210:8080`;
 describe('Atsea System Utilities API tests', () => {
   let response;
   const today = new Date();
-  const today = today.setTime(today.getTime() + 300 * 60 * 1000);
+  const today = today.setTime(today.getTime() - 300 * 60 * 1000);
   const hour = `${today.getHours().toString().padStart(2, '0')}:${today.getMinutes().toString().padStart(2, '0')}`;
   const month = (today.getMonth() + 1).toString().padStart(2, '0');
   const currentDate = `${today.getFullYear()}-${month}-${today.getDate()} ${hour}`;
